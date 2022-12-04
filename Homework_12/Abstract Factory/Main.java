a9dcfc7a16d72085a1244d6f8c655e1322316a36
joshuavh47 <joshuavh47@gmail.com>
2022-12-03 19:11:06 -0500

    synced with class repository

---
+++ Homework_12/Abstract Factory/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        MagicWand f=new MagicWand("Gryffindor");
+        OwlEmporium o=new OwlEmporium("Gryffindor");
+        System.out.println(f.toString());
+        System.out.println(o.toString());
+    }
+}
